9d8dcc6cbd22347ee805822226a3cadd432bc218
kmrin <km.rin@proton.me>
2023-10-23 14:12:26 -0300

    Update init.py

--- init.py
+++ init.py
@@ -231,7 +231,7 @@ async def checkOwners():
 async def on_ready() -> None:
     # Initial logger info (splash)
     bot.logger.info("--------------------------------------")
-    bot.logger.info(" >   RinBot v1.5.1 (GitHub release)   ")
+    bot.logger.info(" >   RinBot v1.6.0 (GitHub release)   ")
     bot.logger.info("--------------------------------------")
     bot.logger.info(f" > Logged as {bot.user.name}")
     bot.logger.info(f" > API Version: {discord.__version__}")
