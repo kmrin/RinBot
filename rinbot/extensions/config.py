e392d62f3c3e7527e0111ef7143ae2824c0116cd
Xikly <107074986+Xikly@users.noreply.github.com>
2024-05-31 02:41:51 +0100

    Included DBColumns in config.py

--- rinbot/extensions/config.py
+++ rinbot/extensions/config.py
@@ -16,6 +16,7 @@ from discord.ext.commands import Cog
 
 from rinbot.base import respond
 from rinbot.base import DBTable
+from rinbot.base import DBColumns
 from rinbot.base import RinBot
 from rinbot.base import Colour
 from rinbot.base import text
